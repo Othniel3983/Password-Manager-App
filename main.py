04d74c2223c0010abb62814e52911d52b001fb44
Othniel Sobeng <othnielsobeng39@gmail.com>
2025-08-26 11:48:30 +0000

    Built the main app by importing the generate password and storage functions from the other files

---
+++ main.py
@@ -0,0 +1,54 @@
+from password_generator import generate_password
+import storage
+
+def menu():
+    print("Welcome to Othniel's Password Manager App")
+
+    while True:
+        print("\n--- Password Manager ---")
+        print("1. Generate Password")
+        print("2. Add Credentials")
+        print("3. Retrieve Credentials")
+        print("4. Delete Credentials")
+        print("5. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            length = int(input("Enter Password Length: "))
+            print("Generate Password: ", generate_password(length))
+
+        elif choice == "2":
+            website = input("Enter website/app name: ").lower()
+            username = input("Enter username/email: ").lower()
+            password = input("Enter password (leave blank to auto-generate): ")
+
+            if not password:
+                password = generate_password()
+            storage.add_credentials(website, username, password)
+            print("Credentials saved successfully!")
+        
+        elif choice == "3":
+            website = input("Enter website/app name: ")
+            credentials = storage.get_credentials(website)
+            if credentials:
+                print(f"Username: {credentials['username']}\nPassword: {credentials['password']}")
+            else:
+                print("No credentials found.")
+        
+        elif choice == "4":
+            website = input("Enter website/app name: ")
+            if storage.delete_credentials(website):
+                print("Credentials deleted successfully.")
+            else:
+                print("No credentials found.")
+        
+        elif choice == "5":
+            print("Exiting... Goodbye")
+            break
+        
+        else:
+            print("Invalid choice, try again!")
+    
+if __name__ == "__main__":
+    menu()
